a208454c2a79499125a59e35232abba76961b6d4
jxy0413 <15011006250@163.com>
2021-02-21 11:09:14 +0800

    commit

--- src/zuopackage2/Code01_MergeSort.java
+++ src/zuopackage2/Code01_MergeSort.java
@@ -1,34 +1,25 @@
 package zuopackage2;
 
 public class Code01_MergeSort {
-    public static void main(String[] args) {
-        int a [] = {1,4,12,3,5,3};
-        process(a,0,a.length-1);
-        for(int i:a){
-            System.out.println(i);
-        }
-    }
-
-    public static void process(int []arr,int left,int right){
+    public static void process(int arr[],int left,int right){
         if(left==right){
             return;
         }
         int mid = left+(right-left>>1);
         process(arr,left,mid);
         process(arr,mid+1,right);
-        mergeSort(arr,left,mid,right);
+        merge(arr,left,mid,right);
     }
-
-    private static void mergeSort(int[] arr, int left, int mid, int right) {
+    public static void merge(int arr[],int left,int mid,int right){
         int temp [] = new int[right-left+1];
         int p1 = left;
         int p2 = mid+1;
         int index = 0;
-        while(p1<=mid&&p2<=right){
-            if(arr[p1]>arr[p2]){
-                temp[index++] = arr[p2++];
-            }else{
+        while (p1<=mid&&p2<=right){
+            if(arr[p1]<arr[p2]){
                 temp[index++] = arr[p1++];
+            }else{
+                temp[index++] = arr[p2++];
             }
         }
         while (p1<=mid){
@@ -37,9 +28,16 @@ public class Code01_MergeSort {
         while (p2<=right){
             temp[index++] = arr[p2++];
         }
-        for(int i=0;i<temp.length;i++){
+        for(int i=0;i<arr.length;i++){
             arr[i+left] = temp[i];
         }
     }
 
+    public static void main(String[] args) {
+        int arr[] = {3,2,5,1,42,53};
+        process(arr,0,arr.length-1);
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
 }
--- src/zuopackage2/Code_quickSort.java
+++ src/zuopackage2/Code_quickSort.java
@@ -1,35 +1,35 @@
-package zuochengyun;
+package zuopackage2;
 
-public class Code00_quickSort {
-    public static void QuickSort(int arr[],int left,int right){
+public class Code_quickSort {
+    public static void quickSort(int arr[],int left,int right){
         if(left>right){
             return;
         }
+        int i = left;
+        int j = right;
         int base = arr[left];
-        int i=left;
-        int j=right;
         while(i<j){
-            while(arr[j]>=base&&i<j){
+            while (i<j&&arr[j]>=base){
                 j--;
             }
-            while(arr[i]<=base&&i<j){
+            while (i<j&&arr[i]<=base){
                 i++;
             }
             if(i<j){
                 int temp = arr[i];
                 arr[i]=arr[j];
-                arr[j]=temp;
+                arr[j] = temp;
             }
         }
         arr[left] = arr[i];
         arr[i] = base;
-        QuickSort(arr,0,i-1);
-        QuickSort(arr,i+1,right);
+        quickSort(arr,0,i-1);
+        quickSort(arr,i+1,right);
     }
 
     public static void main(String[] args) {
         int arr[] ={1,32,45,35,2};
-        QuickSort(arr,0,arr.length-1);
+        quickSort(arr,0,arr.length-1);
         for(int i:arr){
             System.out.println(i);
         }
